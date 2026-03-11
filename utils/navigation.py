ce868f5509a63f305e388d169f3e8af5ab464dcd
Lucas Felipe <lucas.santos@leanfy.com.br>
2026-03-11 14:08:19 -0300

    feat: script clarity  tracker

--- utils/navigation.py
+++ utils/navigation.py
@@ -4,6 +4,17 @@ Componente de Navegação no Topo
 import streamlit as st
 
 def header_menu():
+    # Injeção do Microsoft Clarity
+    st.markdown("""
+        <script type="text/javascript">
+            (function(c,l,a,r,i,t,y){
+                c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
+                t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
+                y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
+            })(window, document, "clarity", "script", "vu6jldw532");
+        </script>
+    """, unsafe_allow_html=True)
+
     st.markdown("""
     <style>
         /* Ocultar barra lateral padrão do Streamlit completamente */
